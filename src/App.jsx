af9efbc59283c494d7796ac270579b49fa848c9b
Isabel Gomez <belasi87@gmail.com>
2021-09-16 11:16:58 +0200

    Create react-app, install all dependencies and create main structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => (
+  <div className="App">
+    <header className="header-container">
+      <h1>Movies</h1>
+    </header>
+    <main className="main-container" />
+  </div>
+);
+
+export default App;
