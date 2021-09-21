97624da1cce2dec9cc3d4f8fc273dd9a6fcbbb4d
Isabel Gomez <belasi87@gmail.com>
2021-09-21 12:16:27 +0200

    Add routes tto navigate to genres

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Header from './components/header/Header';
 import './app.scss';
 
 const App = () => (
   <div className="App">
-    <Header />
-    <main className="main-container">
-      <Home />
-    </main>
+    <Router>
+      <Header />
+      <main className="main-container">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/genre=:genreId" component={Home} />
+        </Switch>
+      </main>
+    </Router>
   </div>
 );
 
