071f2f282ad693377385979f1d1a7ad613b33471
Isabel Gomez <belasi87@gmail.com>
2021-09-21 18:09:11 +0200

    Add logic to create a movie

--- src/components/home/Form.jsx
+++ src/components/home/Form.jsx
@@ -1,27 +1,39 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createMovie } from '../../redux/actions/actionsCreators';
 import Input from '../common/Input/Input';
 import InputTag from '../common/InputTag/InputTag';
 import Button from '../common/Button/Button';
+import imageMovie from '../../assets/movieImg.png';
 import './form.scss';
 
 const Form = () => {
+  const dispatch = useDispatch();
   const [movieName, setMovieName] = useState('');
+  const [selectedTags, setSelectedTags] = useState('');
 
-  const selectedTags = () => {
-    // TODO: implement logic
+  const onSubmitNewMovie = (event) => {
+    event.preventDefault();
+    const newMovie = {
+      image: imageMovie,
+      name: movieName,
+      genres: selectedTags,
+      watched: false,
+    };
+    dispatch(createMovie(newMovie));
   };
 
   return (
-    <form className="form">
+    <form className="form" onSubmit={onSubmitNewMovie}>
       <div className="form-container">
         <label className="form-container__label" htmlFor="title">
           Movie title
-          <Input value={movieName} placeholder="Insert the movie's title and click enter to save" type="text" onChange={(event) => setMovieName(event.target.value)} required />
+          <Input value={movieName} onChange={(event) => setMovieName(event.target.value)} placeholder="Insert the movie's title and click enter to save" type="text" required />
         </label>
         <label className="form-container__label" htmlFor="genres">
           Movie genres
-          <InputTag selectedTags={selectedTags} placeholder="Insert a genre and click enter to save" required />
+          <InputTag selectedTags={setSelectedTags} tags={selectedTags} placeholder="Insert a genre and click enter to save" />
         </label>
         <Button type="submit" className="simple-button" text="Add movie" />
       </div>
