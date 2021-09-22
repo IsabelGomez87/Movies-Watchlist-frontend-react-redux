b2a6ad33382b1cf72075e0387ae976eb14320221
Isabel Gomez <belasi87@gmail.com>
2021-09-22 12:42:38 +0200

    Add list component to home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Form from '../../components/home/Form';
 import Filter from '../../components/home/Filter';
+import List from '../../components/home/List';
 import './home.scss';
 
-const Home = () => (
-  <div className="home-container">
-    <Form />
-    <Filter />
-  </div>
+const Home = () => {
+  const data = useSelector((store) => store.movies);
+  return (
+    <div className="home-container">
+      <Form />
+      <Filter />
+      <List movies={data} />
+    </div>
 
-);
+  );
+};
 
 export default Home;
