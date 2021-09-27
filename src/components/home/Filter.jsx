7738405e7042aeb48054524f4766d564b429f9e8
Isabel Gomez <belasi87@gmail.com>
2021-09-27 18:52:10 +0200

    Add filter and reset functionality

--- src/components/home/Filter.jsx
+++ src/components/home/Filter.jsx
@@ -1,24 +1,15 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState, useEffect } from 'react';
-import { useHistory, useParams } from 'react-router-dom';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 import RadioButton from '../common/RadioButton/RadioButton';
 import Input from '../common/Input/Input';
 import Button from '../common/Button/Button';
 import './filter.scss';
 
-const Filter = () => {
+const Filter = ({ toFilter, onChangeFilter }) => {
   const genresToFilter = ['horror', 'romance', 'comedy'];
-  const { genreId = '' } = useParams();
-  const [toFilter, setToFilter] = useState({ title: '', genre: '' });
   const history = useHistory();
-
-  useEffect(() => {
-    setToFilter({ ...toFilter, genre: genreId });
-  }, [genreId]);
-
-  const onChange = (event) => {
-    setToFilter({ ...toFilter, title: event.target.value });
-  };
 
   return (
     <section className="container">
@@ -39,12 +30,24 @@ const Filter = () => {
         </div>
         <label className="label-container" htmlFor="name">
           Search movie:
-          <Input value={toFilter.title} onChange={onChange} name="name" placeholder="Insert the movie's title" type="text" className="label-container__input" />
+          <Input value={toFilter.name} onChange={onChangeFilter} name="name" placeholder="Insert the movie's title" type="text" className="label-container__input" />
         </label>
         <Button type="button" className="simple-button" text="Reset" handleClick={() => history.push('/')} />
       </div>
     </section>
   );
+};
+
+Filter.propTypes = {
+  toFilter: PropTypes.objectOf(PropTypes.any),
+  onChangeFilter: PropTypes.func,
+
+};
+
+Filter.defaultProps = {
+  toFilter: {},
+  onChangeFilter: null,
+
 };
 
 export default Filter;
