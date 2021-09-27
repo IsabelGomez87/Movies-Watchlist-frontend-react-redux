0155c0e801bb7a333cde58d9ce340fe449cee21e
Isabel Gomez <belasi87@gmail.com>
2021-09-27 13:29:33 +0200

    Sort movies in list

--- src/redux/reducers/moviesReducer.js
+++ src/redux/reducers/moviesReducer.js
@@ -3,13 +3,17 @@ import actionTypes from '../actions/actionTypes';
 const moviesReducer = (movies = [], action) => {
   switch (action.type) {
     case actionTypes.CREATE_MOVIE:
-      return [...movies, action.movie];
+      return [...movies, action.movie]
+        .sort((movie1, movie2) => movie1.movieId - movie2.movieId)
+        .sort((movie1, movie2) => movie1.watched - movie2.watched);
     case actionTypes.DELETE_MOVIE:
       return movies.filter((movie) => movie.movieId !== action.movieId);
     case actionTypes.UPDATE_MOVIE:
       return movies.map((movie) => ((movie.movieId === action.newData.id)
         ? { ...movie, name: action.newData.name, watched: action.newData.watched }
-        : movie));
+        : movie))
+        .sort((movie1, movie2) => movie1.movieId - movie2.movieId)
+        .sort((movie1, movie2) => movie1.watched - movie2.watched);
     default:
       return movies;
   }
